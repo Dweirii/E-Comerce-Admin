467846dd9a86bacb85ab4d0c5a9711a2c4b2531d
Dweiiri <zaiddweiri962@gmail.com>
2025-03-10 15:26:27 +0300

    New

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal-provider";
+
+import { ToasterProvider } from "@/providers/toast-provider";
+
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "An admin dashboard for E-Stores",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ModalProvider/>
+
+          <ToasterProvider/>
+
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
