72dbe1ec8d347b2ff694121833149eb177ef13f5
Dweiiri <zaiddweiri962@gmail.com>
2025-03-11 03:01:18 +0300

    Fix error

--- app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
@@ -16,6 +16,7 @@ import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { AleartModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/use-origin";
 
 
 interface SettingsFormProps {
@@ -36,6 +37,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = (
     const [loading, setLoading] = useState(false);
     const params = useParams();
     const router = useRouter();
+    const origin = useOrigin();
 
   const form = useForm<SettingsFormValues>({
     resolver: zodResolver(formSchema),
